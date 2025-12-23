679b63781a6e709bbef0fd4a7bd6eeec9d8c90b0
javierfajardo01 <javitabara@hotmail.es>
2025-12-23 10:19:00 +0100

    ejercicio 10 vistas parciales

---
+++ Portafolio/Views/Home/_Backend.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portafolio.Views.Home
+{
+    public class _BackendModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
